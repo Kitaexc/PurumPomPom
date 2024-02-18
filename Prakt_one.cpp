679de96c214b77d24c75f2c087414314fdaf362e
Kitaexc <isip_a.p.chebotarev@mpt.ru>
2024-02-18 16:44:54 +0300

    ParamPumPum

---
+++ Prakt_one.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    const int rows = 3;
+    const int cols = 5;
+    double array[rows][cols];
+
+
+    cout << "Введите элементы массива построчно (размером 3x5):\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> array[i][j];
+        }
+    }
+
+
+    for (int i = 0; i < rows; ++i) {
+        double sum = 0;
+        for (int j = 0; j < cols; ++j) {
+            sum += array[i][j];
+        }
+        double average = sum / cols;
+        cout << "Среднее арифметическое элементов " << i + 1 << " строки: "
+            << fixed << setprecision(2) << average << endl;
+    }
+
+    return 0;
+}
